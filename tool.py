a94131b5173292d7af8c25229492b3b49b2bf705
samaha25 <33554014+samaha25@users.noreply.github.com>
2017-12-07 23:27:32 -0500

    final report

--- tool.py
+++ tool.py
@@ -25,21 +25,5 @@ if __name__ == "__main__":
 	a = raw_input("type y for optimized code, n for unoptimized\n")
 	if a == "n":
 		opt = False
-
-	test_file('./final_inputs/p3_input1', opt)
-
-	test_file('./final_inputs/p3_input2', opt)
-
-	test_file('./final_inputs/p3_input3', opt)
-    #
-	test_file('./final_inputs/p3_input4', opt)
-    #
-	test_file('./final_inputs/p3_input5', opt)
-    #
-	test_file('./final_inputs/p3_input6', opt)
-    #
-	test_file('./final_inputs/p3_input7', opt)
-    #
-	test_file('./final_inputs/p3_input8', opt)
-    #
-	test_file('./final_inputs/p3_input9', opt)
+	f = './final_inputs/p3_input9'
+	test_file(f, opt)
